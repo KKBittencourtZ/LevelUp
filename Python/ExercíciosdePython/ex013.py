2f6a2dd56704dd1072ad560b8727146a05e235cb
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-13 16:58:47 -0300

    ex13

---
+++ Python/ExercíciosdePython/ex013.py
@@ -0,0 +1,3 @@
+salario_do_funcionario = float(input('Qual é o salário do funcionário? R$'))
+novo_salario = salario_do_funcionario + salario_do_funcionario * 0.15
+print(f'Um funcionário que ganhava R${salario_do_funcionario:.2f}, com 15% de aumento, passa a receber R${novo_salario:.2f}.')
