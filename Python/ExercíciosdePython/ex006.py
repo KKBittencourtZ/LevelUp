fab5a1447819d8e45a21e96c5126bd285b1a6259
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-13 15:00:03 -0300

    ex6

---
+++ Python/ExercíciosdePython/ex006.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um número: '))
+dobro = numero * 2
+triplo = numero * 3
+rq = numero ** 0.5
+print(
+    f'O dobro de {numero} vale {dobro}.\n'
+    f'O triplo de {numero} vale {triplo}.\n'
+    f'A raiz quadrada de {numero} é igual a {rq:.2f}.'
+)
