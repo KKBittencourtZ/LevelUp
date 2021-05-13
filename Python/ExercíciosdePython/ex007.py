ff1ade8da2b609819b3d6d65bb4295c03b880f8f
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-13 15:12:40 -0300

    ex7

---
+++ Python/ExercíciosdePython/ex007.py
@@ -0,0 +1,4 @@
+nota_1 = float(input('Primeira nota do aluno: '))
+nota_2 = float(input('Segunda nota do aluno: '))
+media = (nota_1 + nota_2) / 2
+print(f'A média entre {nota_1:.1f} e {nota_2:.1f} é igual a {media:.1f}')
