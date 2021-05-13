3c482baf9b4c57e73b65b9ef5eeb20dcf903690a
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-13 11:57:51 -0300

    Conta e inicio da lista de exercício de Python

---
+++ Python/Formulas de Nutri/GlanulometriadeCreatinina.py
@@ -0,0 +1,9 @@
+# 140 é da fórmula
+# 70 é a idade
+# 107,5 é o peso
+# 72 é da fórmula
+# 2.4 é a Creatinina
+meu_nivel = ((140 - 26) * 56.5 / (72 * 0.97))
+x = ((140 - 70) * 107.5) / (72 * 2.4)
+print(x)
+print(meu_nivel)
