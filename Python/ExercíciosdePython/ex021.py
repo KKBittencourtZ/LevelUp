5187f2c3001c2bb05c97b7cf5516ba8b8fcfef72
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-14 01:06:11 -0300

    tentativa fail

--- Python/ExercíciosdePython/ex021.py
+++ Python/ExercíciosdePython/ex021.py
@@ -1,5 +1,5 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('songforpy.mp3')
+pygame.mixer.music.load(r'c:\ProjetctGitHub\LevelUp\Python\ExercíciosdePython\songforpy.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
