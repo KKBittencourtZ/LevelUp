a735e3798d5bf5ee4a59bd4166b81e710d1948df
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-15 19:30:05 -0300

    ex33

---
+++ Python/chalendsPython/ex033.py
@@ -0,0 +1,6 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
+lista = [n1, n2, n3]
+print(f'O menor valor digitado foi {min(lista)}.')
+print(f'O maior valor digitado foi {max(lista)}.')
