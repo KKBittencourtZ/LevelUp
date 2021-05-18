8814661396f989d36c2f48c253dc0237d32afe53
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-17 22:49:16 -0300

    ex53 palíndromo

--- Python/chalendsPython/ex053.py
+++ Python/chalendsPython/ex053.py
@@ -1,1 +1,13 @@
-print('\033[0;30;41mTeste')
+frase = str(input('Digite uma frase: ')).upper().strip()
+junção = frase.split()
+frase_normal = ''
+inverso = ''
+for c in range(0, len(junção)): # Daria para evitar o uso desse laço com frase_normal = ''.join(junção)
+    frase_normal += junção[c]
+for c in range(len(frase_normal) - 1, -1, -1):  # Daria para evitar o uso desse laço com inverso = frase_normal[::-1]
+    inverso += frase_normal[c]
+print(f'O inverso de {frase_normal} é {inverso}')
+if frase_normal == inverso:
+    print('É um palídromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
