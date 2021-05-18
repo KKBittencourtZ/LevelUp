d53923ba40c2edc6142542025a77934eaa152a69
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-18 11:27:22 -0300

    ex72 número extenso

---
+++ Python/chalendsPython/ex072.py
@@ -0,0 +1,6 @@
+numeros = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte']
+while True:
+    numero = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= numero <= 20:
+        break
+print(f'Você digitou o número {numeros[numero]}')
