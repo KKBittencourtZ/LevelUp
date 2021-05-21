cd8795a46714f9f57f820c61a7fbe1026e2ac627
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-05-20 21:43:53 -0300

    ex96

---
+++ Python/chalendsPython/ex096.py
@@ -0,0 +1,9 @@
+def area():
+    print('Controle de Terrenos')
+    print('----------------------')
+    l = float(input('LARGURA: '))
+    c = float(input('COMPRIMENTO: '))
+    print(f'A área de um terreno de {l}x{c} é de {l*c:.1f}m²')
+
+
+area()
