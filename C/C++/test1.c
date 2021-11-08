0aee17cfc683b23b0dcfc16269be6baf7b02c455
César Augusto Bittencourt <bittencourt8154@gmail.com>
2021-11-08 03:25:01 -0300

    Update test1.c
    FULL FAIL

--- C/C++/test1.c
+++ C/C++/test1.c
@@ -1,34 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-void main ( )
+void main (void)
 {
- int diaSemana;
- printf ("Digite o dia da semana: ");
- scanf ("%d", &diaSemana);
- switch (dia) {
-  case 1:
-   printf ("Domingo \n");
-   break;
-  case 2:
-   printf ("Segunda-feira \n");
-   break;
-  case 3:
-   printf ("Terça-feira \n");
-   break;
-  case 4:
-   printf ("Quarta-feira \n");
-   break;
-  case 5:
-   printf ("Quinta-feira \n");
-   break;
-  case 6:
-   printf ("Sexta-feira \n");
-   break;
-  case 7:
-   printf ("Sábado \n");
-   break;
-  default:
-   printf("Dia inválido \n");
-  }
-}
+ int a, b, c;
+ int aux;
+ 
+ printf("Digite três números inteiros: ");
+ }
