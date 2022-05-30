721686c7ea2277d9257e67b0afe552cd4abb3cb2
César Augusto Bittencourt <bittencourt8154@gmail.com>
2022-05-30 13:31:05 -0300

    starting a new project

---
+++ Programinha/LeitorDinamico.py
@@ -0,0 +1,8 @@
+import os
+
+
+mugiwaras = ['Ruffy', 'Zoro', 'Nami', 'Usoop', 'Sanji', 'Chopper', 'Nico Robin', 'Franky', 'Brook', 'Jinbe']
+
+with open('MugiTeam.txt', 'w', newline='') as file:
+    for line in mugiwaras:
+        file.write(line + os.linesep)
