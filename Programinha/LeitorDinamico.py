6be15020f0ab648ccc806a4b22f1fe55cbfa75c3
César Augusto Bittencourt <bittencourt8154@gmail.com>
2022-05-30 13:47:36 -0300

    Successfull !!!

--- Programinha/LeitorDinamico.py
+++ Programinha/LeitorDinamico.py
@@ -3,6 +3,6 @@ import os
 
 mugiwaras = ['Ruffy', 'Zoro', 'Nami', 'Usoop', 'Sanji', 'Chopper', 'Nico Robin', 'Franky', 'Brook', 'Jinbe']
 
-with open('MugiTeam.txt', 'w', newline='') as file:
+with open('Programinha\MugiTeam.txt', 'w', newline='') as file:
     for line in mugiwaras:
         file.write(line + os.linesep)
