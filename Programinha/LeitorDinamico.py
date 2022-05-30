2a60a4c67e115c9f57e837254355cb083a67af94
César Augusto Bittencourt <bittencourt8154@gmail.com>
2022-05-30 13:57:08 -0300

    Starting a new step
    First I tested the library os and now I'll start work with PySimpleGUI

--- Programinha/LeitorDinamico.py
+++ Programinha/LeitorDinamico.py
@@ -1,8 +1,11 @@
 import os
+import PySimpleGUI as pg
+
+
 
 
 mugiwaras = ['Ruffy', 'Zoro', 'Nami', 'Usoop', 'Sanji', 'Chopper', 'Nico Robin', 'Franky', 'Brook', 'Jinbe']
 
-with open('Programinha\MugiTeam.txt', 'w', newline='') as file:
+with open('MugiTeam.txt', 'w', newline='') as file:
     for line in mugiwaras:
         file.write(line + os.linesep)
